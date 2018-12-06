808a506eb6606ae0a62b8637985f2a600a7d7cc0
SineadNaughton <sinead.m.naughton@gmail.com>
2018-12-06 22:44:41 +0000

    -- update for heroku testing

--- src/app/services/auth/auth.constant.ts
+++ src/app/services/auth/auth.constant.ts
@@ -3,5 +3,5 @@ import { isDevMode } from "@angular/core";
 export class AuthConstant {
     public static readonly TOKEN_NAME = 'access_token';
 
-    public static URL_FOR_AUTHENTICATION = (isDevMode) => isDevMode ? 'http://localhost:8080/login' : 'https://vestimony-api.herokuapp.com/login';
+    public static URL_FOR_AUTHENTICATION = (isDevMode) => !isDevMode ? 'http://localhost:8080/login' : 'https://vestimony-api.herokuapp.com/login';
 }
--- src/app/services/vestimony-api/vestimony-api-config.ts
+++ src/app/services/vestimony-api/vestimony-api-config.ts
@@ -1,3 +1,3 @@
 export class VestimonyApiConfig {
-    public static BASE_URL = (isDevMode) => isDevMode ? 'http://localhost:8080' : 'https://vestimony-api.herokuapp.com';
+    public static BASE_URL = (isDevMode) => !isDevMode ? 'http://localhost:8080' : 'https://vestimony-api.herokuapp.com';
 }
